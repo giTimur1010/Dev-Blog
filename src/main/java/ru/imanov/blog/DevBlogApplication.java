2d5430f30a78420b1ce18ce0db06d3b86f1911ae
Imanov Timur <ImanovTB@mpei.ru>
2023-07-15 14:48:02 +0300

    minor changes

--- src/main/java/ru/imanov/blog/DevBlogApplication.java
+++ src/main/java/ru/imanov/blog/DevBlogApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevBlogApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DevBlogApplication.class, args);
 	}
