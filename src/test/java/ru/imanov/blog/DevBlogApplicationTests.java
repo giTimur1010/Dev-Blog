15a8aa3aab71b69722bb8e9c5d07d16bf77f333a
Imanov Timur <ImanovTB@mpei.ru>
2023-07-15 14:45:35 +0300

    basic settings

---
+++ src/test/java/ru/imanov/blog/DevBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.imanov.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
